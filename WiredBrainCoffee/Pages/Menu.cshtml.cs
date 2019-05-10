6a100b17b3e549697e5b7239af3d68bb5a02493f
Arnoldo Perozo <arnoldo.perozo@hotmail.com>
2019-05-10 08:47:59 -0600

    Razor Pages Essential Concepts

--- WiredBrainCoffee/Pages/Menu.cshtml.cs
+++ WiredBrainCoffee/Pages/Menu.cshtml.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
 
 namespace WiredBrainCoffee.Pages
 {
     public class MenuModel : PageModel
     {
+        public List<MenuItem> Menu { get; set; }
+
         public void OnGet()
         {
+            var menuService = new MenuService();
 
+            Menu = menuService.GetMenuItems();
         }
     }
 }
