721d73e6e7b2ec929021731e6b5cb52afa777919
Arnoldo Perozo <arnoldo.perozo@hotmail.com>
2019-05-10 17:24:55 -0600

    Working with Forms, Binding, and Validation

--- WiredBrainCoffee/Models/Contact.cs
+++ WiredBrainCoffee/Models/Contact.cs
@@ -1,10 +1,21 @@
-﻿namespace WiredBrainCoffee.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WiredBrainCoffee.Models
 {
     public class Contact
     {
+        [Required]
         public string Name { get; set; }
+
+        [Phone]
+        [Required]
         public string Phone { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(10)]
         public string Message { get; set; }
     }
 }
